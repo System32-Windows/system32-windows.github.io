c42314de2e0df8eb6919a3ec6fbc0d3d93da9ec0
System32-Windows <pango.land@outlook.com>
2024-03-07 20:01:50 +0100

    Hgfglk

--- timeapi.js
+++ timeapi.js
@@ -1,1 +1,18 @@
-const antin = new Request(`https://timeapi.io/api/TimeZone/AvailableTimeZones`)
+async function getconstantin() {
+    const response = await fetch(`http://worldtimeapi.org/api/timezone`, {
+        mode: "cors"
+    });
+    const movies = await response.json();
+    for (const key in movies) {
+        if (Object.hasOwnProperty.call(movies, key)) {
+            const element = movies[key];
+            document.getElementById("seltime").innerHTML = document.getElementById("seltime").innerHTML + `<option value=${element}>${element}</option>`
+        }
+    }
+}
+async function selected() {
+    const text =
+        const resp2 = await fetch(`http://worldtimeapi.org/api/timezone/${document.getElementById("seltime").value}`)
+    var time = resp2.json()
+    document.getElementById("time").innerText = time["datetime"]
+}
